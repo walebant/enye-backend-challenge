d8366eaff2ad7ea3f44928ef931b7082e6794837
walebant <wale.ashiru04@gmail.com>
2021-01-23 13:16:13 +0100

    feat: filter response by requested currency

--- src/controllers/rates.js
+++ src/controllers/rates.js
@@ -5,28 +5,39 @@ const ratesController = async (req, res) => {
   const { base, currency } = req.query;
   const currencyArray = currency.split(',');
 
-  // fetch data
-  const { data } = await axios.get('https://api.exchangeratesapi.io/latest');
+  try {
+    // fetch data
+    const { data } = await axios.get(
+      `https://api.exchangeratesapi.io/latest?base=${base}`
+    );
 
-  // convert data
-  const rates = Object.fromEntries(
-    Object.entries(data.rates).filter(d => currencyArray.includes(d[0]))
-  );
+    // filter data by requested currency
+    const rates = Object.fromEntries(
+      Object.entries(data.rates).filter(rate => currencyArray.includes(rate[0]))
+    );
 
-  // get date
-  const date = new Date();
-  const dateFormat = `${date.getFullYear()}-${
-    date.getMonth() + 1 <= 9 ? `0${date.getMonth() + 1}` : date.getMonth() + 1
-  }-${date.getDate()}`;
+    // get date
+    const date = new Date();
+    const dateFormat = `${date.getFullYear()}-${
+      date.getMonth() + 1 <= 9 ? `0${date.getMonth() + 1}` : date.getMonth() + 1
+    }-${date.getDate()}`;
 
-  // return result
-  return res.status(200).send({
-    results: {
-      base,
-      date: dateFormat,
-      rates,
-    },
-  });
+    // return result
+    return res.status(200).send({
+      results: {
+        base,
+        date: dateFormat,
+        rates,
+      },
+    });
+  } catch (error) {
+    const { response, message } = error;
+    return res.status(response.status).send({
+      message,
+      statusText: response.statusText,
+      status: response.status,
+    });
+  }
 };
 
 module.exports = ratesController;
